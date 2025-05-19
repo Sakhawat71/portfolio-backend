281f42ecf765bfaf64710491bed055a298a126a1
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-05-20 04:23:14 +0600

    file uploader

--- src/app/utils/fileUploader.ts
+++ src/app/utils/fileUploader.ts
@@ -1,53 +1,97 @@
 import multer from "multer";
 import path from "path";
 import { v2 as cloudinary } from 'cloudinary';
-import fs from "fs";
+import DatauriParser from 'datauri/parser';
 import configs from "../configs";
 
-// Configuration
+// Config Cloudinary
 cloudinary.config({
     cloud_name: configs.cloudinary.cloud_name,
     api_key: configs.cloudinary.api_key,
     api_secret: configs.cloudinary.api_secret
 });
 
+// Memory storage for multer (no need to store on disk)
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, path.join(process.cwd(), '/uploads'))
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-});
+// DataUri parser
+const parser = new DatauriParser();
 
-// console.log(path.join(process.cwd(), '/uploads'));
-const upload = multer({ storage: storage });
+// Upload to Cloudinary from memory
+const uploadToCloudinary = async (file: Express.Multer.File) => {
+    const ext = path.extname(file.originalname);
+    const dataUri = parser.format(ext, file.buffer);
 
-
-const uploadToCloudinary = async (file: any) => {
-
-    // console.log({ file });
     return new Promise((resolve, reject) => {
-        cloudinary.uploader
-            .upload(
-                file.path, {
-                public_id: file.originalname,
-            }, (error, result) => {
-                fs.unlinkSync(file.path)
-                if (error) {
-                    reject(error)
-                } else {
-                    resolve(result)
-                }
-            })
-
-    })
+        cloudinary.uploader.upload(
+            dataUri.content || '',
+            { public_id: path.parse(file.originalname).name },
+            (error, result) => {
+                if (error) reject(error);
+                else resolve(result);
+            }
+        );
+    });
 };
-
-
 
 export const fileUploader = {
     upload,
     uploadToCloudinary
 };
+
+
+
+// import multer from "multer";
+// import path from "path";
+// import { v2 as cloudinary } from 'cloudinary';
+// import fs from "fs";
+// import configs from "../configs";
+
+// // Configuration
+// cloudinary.config({
+//     cloud_name: configs.cloudinary.cloud_name,
+//     api_key: configs.cloudinary.api_key,
+//     api_secret: configs.cloudinary.api_secret
+// });
+
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, path.join(process.cwd(), '/uploads'))
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, file.originalname)
+//     }
+// });
+
+// // console.log(path.join(process.cwd(), '/uploads'));
+// const upload = multer({ storage: storage });
+
+
+// const uploadToCloudinary = async (file: any) => {
+
+//     // console.log({ file });
+//     return new Promise((resolve, reject) => {
+//         cloudinary.uploader
+//             .upload(
+//                 file.path, {
+//                 public_id: file.originalname,
+//             }, (error, result) => {
+//                 fs.unlinkSync(file.path)
+//                 if (error) {
+//                     reject(error)
+//                 } else {
+//                     resolve(result)
+//                 }
+//             })
+
+//     })
+// };
+
+
+
+// export const fileUploader = {
+//     upload,
+//     uploadToCloudinary
+// };
